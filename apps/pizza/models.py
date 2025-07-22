f346f5973b2fd4eb192451c228a0ab964b61f516
Sasha <sasaserbinena@gmail.com>
2025-07-21 23:26:53 -0600

    commit

--- apps/pizza/models.py
+++ apps/pizza/models.py
@@ -8,7 +8,7 @@ from apps.pizza_shop.models import PizzaShopModel
 class PizzaModel(BaseModel):
     class Meta:
         db_table = 'pizza'
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=20, blank=True)
     size = models.IntegerField()
     price= models.FloatField()
     pizza_shop=models.ForeignKey(PizzaShopModel, on_delete=models.CASCADE, related_name='pizzas')
