e9caa9a2a8c2ed0e1604da3a0f299fa6212a063e
Sasha <sasaserbinena@gmail.com>
2025-07-21 23:16:35 -0600

    commit

--- apps/pizza/views.py
+++ apps/pizza/views.py
@@ -1,11 +1,11 @@
-from rest_framework.generics import ListAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListCreateAPIView
 
 from apps.pizza.filter import filter_pizza
 from apps.pizza.models import PizzaModel
 from apps.pizza.serializers import PizzaSerializer
 
 
-class PizzaListCreateView(ListAPIView):
+class PizzaListCreateView(ListCreateAPIView):
     serializer_class = PizzaSerializer
 
     def get_queryset(self):
