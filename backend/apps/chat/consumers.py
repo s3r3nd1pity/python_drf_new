58dfe2377277171f5f7c11885cb514f57eb87e28
Sasha <sasaserbinena@gmail.com>
2025-08-05 13:44:41 -0600

    commit

--- backend/apps/chat/consumers.py
+++ backend/apps/chat/consumers.py
@@ -1,5 +1,6 @@
 from djangochannelsrestframework.generics import GenericAsyncAPIConsumer
 from channels.db import database_sync_to_async
+from djangochannelsrestframework.decorators import action
 
 
 class ChatConsumer(GenericAsyncAPIConsumer):
@@ -13,8 +14,31 @@ class ChatConsumer(GenericAsyncAPIConsumer):
         await self.accept()
         self.room_name = self.scope["url_route"]["kwargs"]["room"]
         self.user_name = await self.get_profile_name()
-        print(self.user_name, self.room_name)
+        await self.channel_layer.group_add(self.room_name, self.channel_name)
+        await self.channel_layer.group_send(
+            self.room_name,
+            {
+                "type":"sender",
+                "message":f'{self.user_name} has joined room {self.room_name}'
+            }
+        )
 
+    async def sender(self, data):
+        print(data)
+        await self.send_json(data)
+
+    @action
+    async def send_message(self, data, request_id, action):
+
+        await self.channel_layer.group_send(
+            self.room_name,
+            {
+                "type":"sender",
+                "message":data,
+                "user":self.user_name,
+                "id":request_id,
+            }
+        )
 
     @database_sync_to_async
     def get_profile_name(self):
--- backend/configs/extra_conf/__init__.py
+++ backend/configs/extra_conf/__init__.py
@@ -2,3 +2,4 @@ from .rest_conf import *
 from .jwt_conf import *
 from .email_conf import *
 from .celery_conf import *
+from .channels_conf import *
